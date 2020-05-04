ba11b6bcdb301abc6d627bde323e352f3916dbbe
Saishyam <Saishyam@192.168.0.103>
2020-05-04 16:22:49 +0530

    Test Class Commit - Sero

---
+++ web-customer-tracker/src/com/luv2code/springdemo/entity/Test.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo.entity;
+
+public class Test {
+
+	private String room1;
+	private String room2;
+	private String room3;
+	private String room4[]= {"A","B","C"};
+	
+	
+}
