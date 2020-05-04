0f9b3616f34edd1ce00686e0ada37dddec2bde23
Saishyam <Saishyam@192.168.0.103>
2020-05-04 16:32:06 +0530

    changes in test class - arey

--- web-customer-tracker/src/com/luv2code/springdemo/entity/Test.java
+++ web-customer-tracker/src/com/luv2code/springdemo/entity/Test.java
@@ -3,8 +3,8 @@ package com.luv2code.springdemo.entity;
 public class Test {
 
 	private String room1;
-	private String room2;
-	private String room3;
+	private int room2;
+	private String room3[]= {"1","2","3"};
 	private String room4[]= {"A","B","C"};
 	
 	
